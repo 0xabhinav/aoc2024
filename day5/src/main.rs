eaeec1c3430279eb3a1bb8de3221f1fd364ffbcb
Kumar Abhinav <97421185+0xabhinav@users.noreply.github.com>
2024-12-05 10:49:41 +0530

    Day5

---
+++ day5/src/main.rs
@@ -0,0 +1,52 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::time::Instant;
+use std::collections::HashSet;
+
+fn solve(file: File) -> u128 {
+    let mut ans = 0;
+    let mut lines = std::io::BufReader::new(file).lines();
+    let mut pairs: Vec<(u64, u64)> = Vec::new();
+
+    // Read until empty line
+    while let Some(line) = lines.by_ref().next() {
+        let line = line.unwrap();
+        if line.is_empty() {
+            break;
+        }
+        // num|num
+        let nums: Vec<u64> = line.split('|').map(|s| s.parse().unwrap()).collect();
+        pairs.push((nums[0], nums[1]));
+    }
+
+    while let Some(line) = lines.by_ref().next() {
+        // parse comma separated numbers
+        let nums: Vec<u64> = line.unwrap().split(',').map(|s| s.parse().unwrap()).collect();
+        let mut seen_set: HashSet<u64> = HashSet::new();
+        let mut valid = true;
+        for num in nums.iter() {
+            for (a, b) in pairs.iter() {
+                if *a == *num && seen_set.contains(b) {
+                    valid = false;
+                    break;
+                }
+            }
+            seen_set.insert(*num);
+        }
+        if valid {
+            let mid_idx = (nums.len() - 1) / 2;
+            ans += nums[mid_idx];
+        }
+    }
+    ans.into()
+}
+
+fn main() {
+    let file = File::open("input.txt").unwrap();
+    // log the time
+    let start = Instant::now();
+    let safe_cnt = solve(file);
+    let duration = start.elapsed();
+    println!("{}", safe_cnt);
+    println!("Time taken: {:?}", duration);
+}
